612eb3db7cdb792d82454c0c8b3c30f655a2bae0
gax <gax@disroot.org>
2023-03-01 18:30:51 +0000

    cliente mqtt con docker

---
+++ clienteMqtt/clienteMqtt.py
@@ -0,0 +1,30 @@
+import asyncio, ssl, certifi, logging
+from asyncio_mqtt import Client, ProtocolVersion
+from environs import Env
+
+env = Env()
+env.read_env() #lee el archivo con las variables. por defecto .env
+
+logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S')
+
+async def main():
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.minimum_version = ssl.TLSVersion.TLSv1_2
+    tls_context.maximum_version = ssl.TLSVersion.TLSv1_3
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+
+    async with Client(
+        env("SERVIDOR"),
+        protocol=ProtocolVersion.V31,
+        port=8883,
+        tls_context=tls_context,
+    ) as client:
+        async with client.messages() as messages:
+            await client.subscribe("#")
+            async for message in messages:
+                logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
