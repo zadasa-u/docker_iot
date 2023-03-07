e43ab360a5c51f7e0b642d24e8138101c2ab0de0
gax <gax@disroot.org>
2023-03-07 14:38:50 -0300

    mi primer bot

---
+++ telegrambot/telegrambot.py
@@ -0,0 +1,36 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import logging, os
+
+token=os.getenv("TOKEN")
+
+logging.basicConfig(format='%(asctime)s - TelegramBot - %(levelname)s - %(message)s', level=logging.INFO)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info("se conectó: " + str(update.message.from_user.id))
+    if update.message.from_user.first_name:
+        nombre=update.message.from_user.first_name
+    else:
+        nombre=""
+    if update.message.from_user.last_name:
+        apellido=update.message.from_user.last_name
+    else:
+        apellido=""
+
+    await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido)
+    # await update.message.reply_text("Bienvenido al Bot "+ nombre + " " + apellido) # también funciona
+
+async def acercade(update: Update, context):
+    await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT (2023)")
+
+
+def main() -> None:
+    application = Application.builder().token(token).build()
+
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('acercade', acercade))
+
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
