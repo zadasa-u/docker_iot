b21165c356aebb223f821c4863888ef3fc0f1f0b
gax <gax@disroot.org>
2023-05-01 19:42:03 -0300

    asyncio

--- apiiot/app.py
+++ apiiot/app.py
@@ -1,27 +1,36 @@
-import asyncio
+import asyncio, logging
 from fastapi import FastAPI
-from sqlalchemy import create_engine, MetaData, select, URL
+from sqlalchemy import MetaData, select, URL, Table
 from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+
+logging.basicConfig(format='%(asctime)s - apiiot - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 app = FastAPI(root_path="/api")
 url_object = URL.create(
-    "mysql+pymysql",
+    "mysql+aiomysql",
     username=os.environ["MARIADB_USER"],
     password=os.environ["MARIADB_USER_PASS"],
     host=os.environ["MARIADB_SERVER"],
     database=os.environ["MARIADB_DB"],
 )
-engine = create_engine(url_object)
-Session = sessionmaker(engine)
+engine = create_async_engine(url_object)
 meta = MetaData()
-meta.reflect(bind=engine)
-mediciones=meta.tables[os.environ["MARIADB_TABLE"]]
 
 @app.get("/ultimos")
-def ultima():
-    with Session() as session:
-        ultimos = session.execute(select(mediciones).order_by(mediciones.c.id.desc())).first()
-        columnas=[c.name for c in mediciones.columns]
-        ultimos_zip=zip(columnas,ultimos)
-        return ultimos_zip
+async def ultima():
+    async with engine.connect() as conn:
+        await conn.run_sync(meta.reflect, only=[os.environ["MARIADB_TABLE"]])
+        tabla = Table(os.environ["MARIADB_TABLE"], meta, autoload_with=engine)
+        async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+        async with async_session() as session:
+            resultado = await session.execute(select(tabla).order_by(tabla.c.id.desc()))
+            ultimos=resultado.first()
+            columnas=[c.name for c in tabla.columns]
+            ultimos_zip=zip(columnas,ultimos)
+            logging.info(ultimos)
+    await engine.dispose()
+    return ultimos_zip
+    
--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -24,8 +24,6 @@ async def main():
             await client.subscribe('#')
             async for message in messages:
                 logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-                datos=json.loads(message.payload.decode('utf8'))
-                sql = "INSERT INTO `mediciones` (`sensor_id`, `temperatura`, `humedad`) VALUES (%s, %s, %s)"
                 try:
                     conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
                                                 user=os.environ["MARIADB_USER"],
@@ -34,15 +32,31 @@ async def main():
                 except Exception as e:
                     logging.error(traceback.format_exc())
 
-                async with conn.cursor() as cur:
-                    try:
-                        await cur.execute(sql, (message.topic, datos['temperatura'], datos['humedad']))
-                        await conn.commit()
-                        await cur.close()
-                        await conn.ensure_closed()
-                    except Exception as e:
-                        logging.error(traceback.format_exc())
-                        # Logs the error appropriately. 
+                if message.topic.matches("sensores_remotos/#"):
+                    datos=json.loads(message.payload.decode('utf8'))
+                    sql = "INSERT INTO `mediciones` (`sensor_id`, `temperatura`, `humedad`) VALUES (%s, %s, %s)"
+                    async with conn.cursor() as cur:
+                        try:
+                            await cur.execute(sql, (str(message.topic).split('/')[1], datos['temperatura'], datos['humedad']))
+                            await conn.commit()
+                            await cur.close()
+                            await conn.ensure_closed()
+                        except Exception as e:
+                            logging.error(traceback.format_exc())
+                            # Logs the error appropriately. 
+
+                if message.topic.matches("/stackberry/temperatura/+/cpu"):
+                    logging.info(str(message.topic).split('/')[3]+ ": " +message.payload.decode('utf8'))
+                    sql = "INSERT INTO `temp_cpus` (`cpu`, `temperatura`) VALUES (%s, %s)"
+                    async with conn.cursor() as cur:
+                        try:
+                            await cur.execute(sql, (str(message.topic).split('/')[3], message.payload.decode('utf8')))
+                            await conn.commit()
+                            await cur.close()
+                            await conn.ensure_closed()
+                        except Exception as e:
+                            logging.error(traceback.format_exc())
+                            # Logs the error appropriately. 
 
 if __name__ == "__main__":
     asyncio.run(main())
