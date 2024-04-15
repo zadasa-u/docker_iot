6ded9fe60c620540b02f10dce58c85f8affa91e5
Germán Xander <german.xander@fio.unam.edu.ar>
2024-04-15 10:38:39 -0300

    upgrade to aiomqtt

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,26 +1,22 @@
 import asyncio, ssl, certifi, logging, os
-from asyncio_mqtt import Client, ProtocolVersion
+import aiomqtt
 
 logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S %z')
 
 async def main():
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    tls_context.minimum_version = ssl.TLSVersion.TLSv1_2
-    tls_context.maximum_version = ssl.TLSVersion.TLSv1_3
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
 
-    async with Client(
+    async with aiomqtt.Client(
         os.environ['SERVIDOR'],
-        protocol=ProtocolVersion.V31,
         port=8883,
         tls_context=tls_context,
     ) as client:
-        async with client.messages() as messages:
-            await client.subscribe(os.environ['TOPICO'])
-            async for message in messages:
-                logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+        await client.subscribe(os.environ['TOPICO'])
+        async for message in client.messages:
+            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
 
 if __name__ == "__main__":
     asyncio.run(main())
