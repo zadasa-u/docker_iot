21464c32c93930074697ea1ff1c1bd890785a45b
Germán Xander <german.xander@fio.unam.edu.ar>
2024-04-15 09:24:27 -0300

    update to aiomqtt

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,5 +1,5 @@
 import asyncio, ssl, certifi, logging
-from asyncio_mqtt import Client, ProtocolVersion
+import aiomqtt
 from environs import Env
 
 env = Env()
@@ -9,22 +9,18 @@ logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message
 
 async def main():
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    tls_context.minimum_version = ssl.TLSVersion.TLSv1_2
-    tls_context.maximum_version = ssl.TLSVersion.TLSv1_3
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
 
-    async with Client(
+    async with aiomqtt.Client(
         env("SERVIDOR"),
-        protocol=ProtocolVersion.V31,
         port=8883,
         tls_context=tls_context,
     ) as client:
-        async with client.messages() as messages:
-            await client.subscribe("#")
-            async for message in messages:
-                logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+        await client.subscribe("#")
+        async for message in client.messages:
+            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
 
 if __name__ == "__main__":
     asyncio.run(main())
