d81340e0c255ffcdc4851ed16bbc54b6ae13fbe0
zadasa-u <danielzakowicz007@gmail.com>
2024-04-17 09:45:45 -0300

    publica contador. logging actualizado

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -5,7 +5,31 @@ from environs import Env
 env = Env()
 env.read_env() #lee el archivo con las variables. por defecto .env
 
+TP = int(env("TP"))
+
 logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S')
+
+contador = 0
+
+async def task():
+    global contador
+    while True:
+        contador += 1
+        logging.info('coro: task - ' + 'count: ' + str(contador))
+        await asyncio.sleep(3)
+
+async def receive(client):
+    await client.subscribe(env("TOPICO1"))
+    await client.subscribe(env("TOPICO2"))
+    async for message in client.messages:
+        logging.info('coro: receive - ' + str(message.topic) + ": " + message.payload.decode("utf-8"))
+
+async def publish(client):
+    global contador
+    while True:
+        await client.publish(env("PUBLICAR"), str(contador))
+        logging.info('coro: publish - ' + 'topic: ' + env("PUBLICAR") + ' - payload: ' + str(contador))
+        await asyncio.sleep(TP)
 
 async def main():
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -18,9 +42,7 @@ async def main():
         port=8883,
         tls_context=tls_context,
     ) as client:
-        await client.subscribe("#")
-        async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+        await asyncio.gather(receive(client),publish(client), task())
 
 if __name__ == "__main__":
     asyncio.run(main())
