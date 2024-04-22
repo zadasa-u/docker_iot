3aceb1de3c866dac1e515790977a54e51a47a0b7
zadasa-u <danielzakowicz007@gmail.com>
2024-04-22 17:55:58 -0300

    Implementando colas para recibir y consumir mensajes

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -11,22 +11,35 @@ logging.basicConfig(format='%(asctime)s - cliente mqtt - %(taskName)s => %(level
 
 cuenta = 0
 
-async def task():
+async def counter():
     global cuenta
     while True:
         cuenta += 1
-        logging.info('Valor contador: ' + str(cuenta))
+        logging.info('Valor cuenta: ' + str(cuenta))
         await asyncio.sleep(3)
 
-async def receive(client):
-    async for message in client.messages:
+async def topic1_consumer():
+    while True:
+        message = await topic1_queue.get()
         logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+
+async def topic2_consumer():
+    while True:
+        message = await topic2_queue.get()
+        logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+
+async def distributor(client):
+    async for message in client.messages:
+        if message.topic.matches(env('TOPICO1')):
+            topic1_queue.put_nowait(message)
+        elif message.topic.matches(env('TOPICO2')):
+            topic2_queue.put_nowait(message)
 
 async def publish(client):
     global cuenta
     while True:
         await client.publish(env("PUBLICAR"), str(cuenta))
-        logging.info('topic: ' + env("PUBLICAR") + ' - payload: ' + str(cuenta))
+        logging.info('topico: ' + env("PUBLICAR") + ' - contenido: ' + str(cuenta))
 
         await asyncio.sleep(TP)
 
@@ -45,13 +58,17 @@ async def main():
         await client.subscribe(env("TOPICO2"))
         
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(receive(client), name='receive')
-            tg.create_task(publish(client), name='publish')
-            tg.create_task(task(), name='contador')
-        
-        #await asyncio.gather(receive(client),publish(client), task())
+            tg.create_task(distributor(client), name='distribucion')
+            tg.create_task(topic1_consumer(), name='consumir topico 1')
+            tg.create_task(topic2_consumer(), name='consumir topico 2')
+            tg.create_task(publish(client), name='publicacion')
+            tg.create_task(counter(), name='conteo')
 
 if __name__ == "__main__":
+
+    topic1_queue = asyncio.Queue()
+    topic2_queue = asyncio.Queue()
+    
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
