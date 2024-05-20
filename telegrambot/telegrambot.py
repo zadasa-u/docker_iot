40d7c67c0ddf69954fadedb5fd229a22d10346a9
Germán Xander <german.xander@fio.unam.edu.ar>
2024-05-20 16:09:06 -0300

    ver2024b

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import logging, os
+import logging, os, asyncio
 
 token=os.environ["TB_TOKEN"]
 
@@ -25,7 +25,7 @@ async def acercade(update: Update, context):
 async def kill(update: Update, context):
     logging.info(context.args)
     if context.args and context.args[0] == '@e':
-        await context.bot.send_animation(update.message.chat.id, "CgACAgEAAxkBAANXZAiWvDIEfGNVzodgTgH1o5z3_WEAAmUCAALrx0lEZ8ytatzE5X0uBA")
+        await context.bot.send_animation(update.message.chat.id, "CgACAgEAAxkBAAOPZkuctzsWZVlDSNoP9PavSZmH5poAAmUCAALrx0lEVKaX7K-68Ns1BA")
         await asyncio.sleep(6)
         await context.bot.send_message(update.message.chat.id, text="¡¡¡Ahora estan todos muertos!!!")
     else:
