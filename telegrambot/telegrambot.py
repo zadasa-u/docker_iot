674bf9132d142496bf7218e01b801da9468b2de8
Germán Xander <german.xander@fio.unam.edu.ar>
2024-05-23 10:08:51 -0300

    ver2024

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -1,6 +1,6 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-import logging, os, asyncio
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import logging, os, asyncio, aiomysql, traceback, locale
 
 token=os.environ["TB_TOKEN"]
 
@@ -16,8 +16,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         apellido=update.message.from_user.last_name
     else:
         apellido=""
-    await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido)
-    # await update.message.reply_text("Bienvenido al Bot "+ nombre + " " + apellido) # también funciona
+    kb = [["temperatura"],["humedad"]]
+    await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido,reply_markup=ReplyKeyboardMarkup(kb))
 
 async def acercade(update: Update, context):
     await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT FIO")
@@ -31,11 +31,32 @@ async def kill(update: Update, context):
     else:
         await context.bot.send_message(update.message.chat.id, text="☠️ ¡¡¡Esto es muy peligroso!!! ☠️")
         
+async def medicion(update: Update, context):
+    logging.info(update.message.text)
+    sql = f"SELECT timestamp, {update.message.text} FROM mediciones ORDER BY timestamp DESC LIMIT 1"
+    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
+                                    user=os.environ["MARIADB_USER"],
+                                    password=os.environ["MARIADB_USER_PASS"],
+                                    db=os.environ["MARIADB_DB"])
+    async with conn.cursor() as cur:
+        await cur.execute(sql)
+        r = await cur.fetchone()
+        if update.message.text == 'temperatura':
+            unidad = 'ºC'
+        else:
+            unidad = '%'
+        await context.bot.send_message(update.message.chat.id,
+                                    text="La última {} es de {} {},\nregistrada a las {:%H:%M:%S %d/%m/%Y}"
+                                    .format(update.message.text, str(r[1]).replace('.',','), unidad, r[0]))
+        logging.info("La última {} es de {} {}, medida a las {:%H:%M:%S %d/%m/%Y}".format(update.message.text, r[1], unidad, r[0]))
+    conn.close()
+
 def main():
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('acercade', acercade))
     application.add_handler(CommandHandler('kill', kill))
+    application.add_handler(MessageHandler(filters.Regex("^(temperatura|humedad)$"), medicion))
     application.run_polling()
 
 if __name__ == '__main__':
