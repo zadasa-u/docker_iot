66e99057cf3d2f21fcc64e790844587b7abcb291
zadasa-u <danielzakowicz007@gmail.com>
2024-06-06 15:53:24 -0300

    coma

--- crud/crud.py
+++ crud/crud.py
@@ -121,7 +121,7 @@ def borrar_contacto(id):
 @require_login
 def conseguir_contacto(id):
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM contactos WHERE id = %s', (id))
+    cur.execute('SELECT * FROM contactos WHERE id = %s', (id,))
     datos = cur.fetchone()
     logging.info(datos)
     return render_template('editar-contacto.html', contacto = datos)
