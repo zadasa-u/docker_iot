481041d0013e1d0f88a22da05301c66d996f3e9c
zadasa-u <danielzakowicz007@gmail.com>
2024-07-05 11:15:54 -0300

    Lineas umbral en graficas

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -6,6 +6,17 @@ from io import BytesIO
 
 token=os.environ["TB_TOKEN"]
 
+MARIADB_SERVER = os.environ["MARIADB_SERVER"]
+MARIADB_USER = os.environ["MARIADB_USER"]
+MARIADB_USER_PASS = os.environ["MARIADB_USER_PASS"]
+MARIADB_DB = os.environ["MARIADB_DB"]
+MARIADB_TABLE = os.environ["MARIADB_TABLE"]
+
+MIN_TEMP = os.environ["MIN_TEMP"]
+MAX_TEMP = os.environ["MAX_TEMP"]
+OPT_MIN_TEMP = os.environ["OPT_MIN_TEMP"]
+OPT_MAX_TEMP = os.environ["OPT_MAX_TEMP"]
+
 logging.basicConfig(format='%(asctime)s - TelegramBot - %(levelname)s - %(message)s', level=logging.INFO)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -19,27 +30,24 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         apellido=""
     kb = [["temperatura"],["humedad"],["gráfico temperatura"],["gráfico humedad"]]
-    await context.bot.send_message(update.message.chat.id, text="Bienvenido al Bot "+ nombre + " " + apellido,reply_markup=ReplyKeyboardMarkup(kb))
-
-async def acercade(update: Update, context):
-    await context.bot.send_message(update.message.chat.id, text="Este bot fue creado para el curso de IoT FIO")
-
-async def kill(update: Update, context):
-    logging.info(context.args)
-    if context.args and context.args[0] == '@e':
-        await context.bot.send_animation(update.message.chat.id, "CgACAgEAAxkBAAOPZkuctzsWZVlDSNoP9PavSZmH5poAAmUCAALrx0lEVKaX7K-68Ns1BA")
-        await asyncio.sleep(6)
-        await context.bot.send_message(update.message.chat.id, text="¡¡¡Ahora estan todos muertos!!!")
-    else:
-        await context.bot.send_message(update.message.chat.id, text="☠️ ¡¡¡Esto es muy peligroso!!! ☠️")
+    await context.bot.send_message(
+        update.message.chat.id,
+        text="Bienvenido al Bot "+ nombre + " " + apellido,
+        reply_markup=ReplyKeyboardMarkup(kb)
+    )
         
 async def medicion(update: Update, context):
     logging.info(update.message.text)
-    sql = f"SELECT timestamp, {update.message.text} FROM mediciones ORDER BY timestamp DESC LIMIT 1"
-    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                    user=os.environ["MARIADB_USER"],
-                                    password=os.environ["MARIADB_USER_PASS"],
-                                    db=os.environ["MARIADB_DB"])
+    columna = update.message.text
+    
+    sql = f"SELECT timestamp, {columna} FROM {MARIADB_TABLE} ORDER BY timestamp DESC LIMIT 1"
+    conn = await aiomysql.connect(
+        host=MARIADB_SERVER,
+        port=3306,
+        user=MARIADB_USER,
+        password=MARIADB_USER_PASS,
+        db=MARIADB_DB
+    )
     async with conn.cursor() as cur:
         await cur.execute(sql)
         r = await cur.fetchone()
@@ -47,26 +55,37 @@ async def medicion(update: Update, context):
             unidad = 'ºC'
         else:
             unidad = '%'
-        await context.bot.send_message(update.message.chat.id,
-                                    text="La última {} es de {} {},\nregistrada a las {:%H:%M:%S %d/%m/%Y}"
-                                    .format(update.message.text, str(r[1]).replace('.',','), unidad, r[0]))
-        logging.info("La última {} es de {} {}, medida a las {:%H:%M:%S %d/%m/%Y}".format(update.message.text, r[1], unidad, r[0]))
+        text = "La última {} es de {} {},\nregistrada a las {:%H:%M:%S %d/%m/%Y}".format(update.message.text, str(r[1]).replace('.',','), unidad, r[0])
+        await context.bot.send_message(update.message.chat.id,text=text)
+        logging.info(text)
     conn.close()
 
 async def graficos(update: Update, context):
     logging.info(update.message.text)
-    sql = f"SELECT timestamp, {update.message.text.split()[1]} FROM mediciones where id mod 2 = 0 AND timestamp >= NOW() - INTERVAL 1 DAY ORDER BY timestamp"
-    conn = await aiomysql.connect(host=os.environ["MARIADB_SERVER"], port=3306,
-                                    user=os.environ["MARIADB_USER"],
-                                    password=os.environ["MARIADB_USER_PASS"],
-                                    db=os.environ["MARIADB_DB"])
+    columna = update.message.text.split()[1]
+    sql = f"SELECT timestamp, {columna} FROM {MARIADB_TABLE} where id mod 2 = 0 AND timestamp >= NOW() - INTERVAL 1 DAY ORDER BY timestamp"
+    conn = await aiomysql.connect(
+        host=MARIADB_SERVER,
+        port=3306,
+        user=MARIADB_USER,
+        password=MARIADB_USER_PASS,
+        db=MARIADB_DB
+    )
     async with conn.cursor() as cur:
         await cur.execute(sql)
         filas = await cur.fetchall()
 
         fig, ax = plt.subplots(figsize=(7, 4))
         fecha,var=zip(*filas)
-        ax.plot(fecha,var)
+        ax.plot(fecha,var,color='k', linewidth=2)
+        if columna == "temperatura":
+            ax.set_ylim([0,50])
+            ax.axhline(y = MIN_TEMP, color = 'r', linestyle = '--', linewidth=2)
+            ax.axhline(y = OPT_MIN_TEMP, color = 'y', linestyle = '--', linewidth=2)
+            ax.axhline(y = OPT_MAX_TEMP, color = 'y', linestyle = '--', linewidth=2)
+            ax.axhline(y = MAX_TEMP, color = 'r', linestyle = '--', linewidth=2)
+        else:
+            ax.set_ylim([0,100])
         ax.grid(True, which='both')
         ax.set_title(update.message.text, fontsize=14, verticalalignment='bottom')
         ax.set_xlabel('fecha')
@@ -82,8 +101,6 @@ async def graficos(update: Update, context):
 def main():
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('acercade', acercade))
-    application.add_handler(CommandHandler('kill', kill))
     application.add_handler(MessageHandler(filters.Regex("^(temperatura|humedad)$"), medicion))
     application.add_handler(MessageHandler(filters.Regex("^(gráfico temperatura|gráfico humedad)$"), graficos))
     application.run_polling()
